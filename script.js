f1bb53c980475a79ff56a069588b2b74ee0f6b0a
zhanc638 <47405683+zhanc638@users.noreply.github.com>
2022-01-10 21:53:25 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let aboutButton = document.getElementById("aboutButton")
+let homeButton = document.getElementById("nameButton")
+let portfolio = document.getElementById("portfolio")
+let aboutPage = document.getElementById("about")
+
+aboutButton.addEventListener("click", function(){
+  portfolio.style.display = "none"
+  aboutPage.style.display = "block"
+})
+homeButton.addEventListener("click", function(){
+  portfolio.style.display = "block"
+  aboutPage.style.display = "none"
+})
+
+
+
+//Expandig images
+
+let projects = document.querySelectorAll("div.projects")
+
+projects.forEach((project, i) => {
+  let imagesToExpand = project.querySelectorAll("img.expand")
+  let details = project.querySelector("details")
+  let summary = project.querySelector("summary")
+
+  summary.addEventListener("click", function(){
+    if (details.open == true) {
+      imagesToExpand.forEach((image, i) => {
+        image.style.display = "none"
+      })
+    }
+    if (details.open == false) {
+      imagesToExpand.forEach((image, i) => {
+        image.style.display = "block"
+      })
+    }
+    console.log(details.classList.contains("open"));
+  })
+
+});
